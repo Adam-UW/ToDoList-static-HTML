8a8b5885c700f6bbee999d6f10e5c3c93add8317
Adam Shandi <abbashasan91@gmail.com>
2020-10-22 21:14:36 -0700

    adding some CSS

---
+++ app.js
@@ -0,0 +1,56 @@
+         // Create a "close" button and append it to each list item
+              var myNodelist = document.getElementsByTagName("LI");
+              var i;
+              console.log(myNodelist.length)
+              for (i = 0; i < myNodelist.length; i++) {
+                  var span= document.createElement("SPAN")
+                  var txt= document.createTextNode("\u00D7")
+                  span.className="close"
+                  span.appendChild(txt)
+                  myNodelist[i].appendChild(span)
+              }
+              var close= document.getElementsByClassName('close')
+              var i
+              for(i=0; i < close.length;i++){
+                  close[i].onClick = ()=>{
+                      var div= this.parentElement
+                      console.log(div)
+                      div.style.display="node"
+                  }
+              }
+
+              var list=document.querySelector('ul')
+              list.addEventListener('click', (e)=>{
+                  if(e.target.tagName === 'LI'){
+                      e.target.classList.toggle('checked')
+                  }
+              }, false)
+
+          function newElement(){
+              var li= document.createElement('li')
+              var inputValue= document.getElementById('myInput').value
+              var t = document.createTextNode(inputValue)
+              li.appendChild(t)
+
+              if(inputValue === ''){
+                  alert('You must write something')
+              } else {
+                  document.getElementById('myUL').appendChild(li)
+              }
+             
+
+              var span = document.createElement('SPAN')
+              var txt = document.createTextNode("\u00D7");
+
+              span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+
+        for (i = 0; i < close.length; i++) {
+          close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+          }
+        }
+      }
